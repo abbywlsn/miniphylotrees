a3eefd0c740666aa22401c21ed3c799407351266
abbywlsn <abbysk8@gmail.com>
2021-07-08 16:33:22 -0700

    added random seed

--- PressureForDiversity.cpp
+++ PressureForDiversity.cpp
@@ -36,6 +36,7 @@ EMP_BUILD_CONFIG(PressureForDiversityConfig,
   VALUE(MUTATION_RATE, double, 0.05, "This is my mutation rate."),
   VALUE(POP_SIZE, int, 10, "This is my population size."),
   VALUE(GENERATION_COUNT, int, 10, "This is my number of generations."),
+  VALUE(RANDOM_SEED, int, -1, "This is my random seed"),
 );
 
 PressureForDiversityConfig config; 
@@ -146,7 +147,7 @@ int main(int argc, char * argv[]) {
     config.Write(std::cout);
     std::cout << "==============================\n" << std::endl; 
 
-    emp::Random randNum;
+    emp::Random randNum(config.RANDOM_SEED());
 
     for(int gen = 1; gen <= 5000; gen++){ 
         for(int run = 1; run <= 1000; run++){ 
